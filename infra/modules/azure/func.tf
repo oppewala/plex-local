5a858a94e9bb107bf72b704e6fb884f5de6dc90d
Arend Oppewal <ao@crackedjar.com>
2021-11-17 22:35:21 +1100

    Add missing variables. Mark as secure to not leak.

--- infra/modules/azure/func.tf
+++ infra/modules/azure/func.tf
@@ -1,6 +1,7 @@
 variable "group_name" {
-  default = ""
-  type    = string
+  default   = ""
+  type      = string
+  sensitive = true
 }
 
 terraform {
--- infra/root.tf
+++ infra/root.tf
@@ -1,6 +1,7 @@
 variable "azure_group_name" {
   type        = string
   description = "Name that will be used to generate resource group and resources"
+  sensitive   = true
 }
 
 
