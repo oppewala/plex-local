a7c4c015b25c669c5e60f7da7415ef5f8376fd4a
Arend Oppewal <ao@crackedjar.com>
2021-11-18 13:24:25 +1100

    Key Vault for secrets

--- infra/modules/azure/func.tf
+++ infra/modules/azure/func.tf
@@ -30,11 +30,19 @@ resource "azurerm_resource_group" "rg" {
   location = "Australia East"
 }
 
-resource "azurerm_application_insights" "fapp" {
+resource "azurerm_application_insights" "core" {
   name                = "ai-${var.group_name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
+}
+
+resource "azurerm_key_vault" "core" {
+  name                = "kv-${var.group_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
 }
 
 # Function App
@@ -71,14 +79,23 @@ resource "azurerm_function_app" "fapp" {
     "WEBSITE_RUN_FROM_PACKAGE"       = ""
     "FUNCTIONS_WORKER_RUNTIME"       = "node"
     "WEBSITE_NODE_DEFAULT_VERSION"   = "10.14.1"
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.fapp.instrumentation_key
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.core.instrumentation_key
   }
   identity {
     type = "SystemAssigned"
   }
 }
 
-# Storage Account Queue
+resource "azurerm_key_vault_access_policy" "fapp" {
+  key_vault_id = azurerm_key_vault.core.id
+  object_id    = azurerm_function_app.fapp.identity.principal_id
+  tenant_id    = azurerm_function_app.fapp.identity.tenant_id
+
+  key_permissions    = ["Get"]
+  secret_permissions = ["Get"]
+}
+
+# Storage Account
 resource "azurerm_storage_account" "sa" {
   name                     = "sa${local.safe_group_name}"
   location                 = azurerm_resource_group.rg.location
@@ -97,6 +114,19 @@ resource "azurerm_storage_table" "sa" {
   storage_account_name = azurerm_storage_account.sa.name
 }
 
-output "sa_conn" {
-  value = azurerm_storage_account.sa.primary_access_key
+resource "azurerm_storage_container" "sa" {
+  name                 = "webhook-requests"
+  storage_account_name = azurerm_storage_account.sa.name
+}
+
+resource "azurerm_key_vault_secret" "sa" {
+  name         = "sa-connection-string"
+  value        = azurerm_storage_account.sa.primary_connection_string
+  key_vault_id = azurerm_key_vault.core.id
+}
+
+resource "azurerm_key_vault_secret" "sa" {
+  name         = "sa-access-key"
+  value        = azurerm_storage_account.sa.primary_access_key
+  key_vault_id = azurerm_key_vault.core.id
 }
